b3be406c436d3c6a65a3e6fea15d1acba96ef65c
Harry Guillermo <hguillermo@fastcompany.com>
2014-06-05 16:41:30 -0700

    made some changes to test the Intimidate library

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var createQueue = require('queue-async')
   , mime = require('mime')
   , once = require('once')
   , knox = require('knox')
+  , Intimidate = require('intimidate')
   , url = require('url')
   , fs = require('fs')
 
@@ -22,8 +23,11 @@ function s3syncer(db, options) {
   options.cacheSrc = options.cacheSrc || __dirname + '/.sync'
   options.cacheDest = options.cacheDest || '/.sync'
   options.retries = options.retries || 7;
+  options.maxRetries = 20;
+  options.backoffInterval = 20;
 
-  var client = knox.createClient(options)
+  // var client = knox.createClient(options)
+  var client = new Intimidate(options)
     , queue = createQueue(options.concurrency)
     , region = options.region === 'us-standard' ? false : options.region
     , secure = options.secure || !('secure' in options)
@@ -60,40 +64,45 @@ function s3syncer(db, options) {
         + options.bucket
         + '/' + relative
 
-    hashFile(absolute, destination, function(err, md5) {
-      if (err) return next(err)
-      details.md5 = md5
-      details.url = destination
-      details.fresh = false
-      details.cached = false
+    details.url = destination
 
-      if (!db) return checkForUpload(next)
+    return checkForUpload(next)
 
-      var key = 'md5:' + hashkey(details)
+    // hashFile(absolute, destination, function(err, md5) {
+    //   if (err) return next(err)
+    //   details.md5 = md5
+    //   details.url = destination
+    //   details.fresh = false
+    //   details.cached = false
 
-      db.get(key, function(err, result) {
-        if (!err && result === md5) {
-          details.cached = true
-          return next(null, details)
-        }
-        checkForUpload(function(err) {
-          if (err) return next(err)
-          db.put(key, md5, next)
-        })
-      })
-    })
+    //   if (!db) return checkForUpload(next)
+
+    //   var key = 'md5:' + hashkey(details)
+
+    //   db.get(key, function(err, result) {
+    //     if (!err && result === md5) {
+    //       details.cached = true
+    //       return next(null, details)
+    //     }
+    //     checkForUpload(function(err) {
+    //       if (err) return next(err)
+    //       db.put(key, md5, next)
+    //     })
+    //   })
+    // })
 
     function checkForUpload(next) {
-      client.headFile(relative, function(err, res) {
-        if (err) return next(err)
-        if (
-          res.statusCode === 404 || (
-          res.headers['x-amz-meta-syncfilehash'] &&
-          res.headers['x-amz-meta-syncfilehash'] !== details.md5
-        )) return uploadFile(details, next)
-        if (res.statusCode >= 300) return next(new Error('Bad status code: ' + res.statusCode))
-        return next(null, details)
-      })
+      return uploadFile(details, next)
+      // client.headFile(relative, function(err, res) {
+      //   if (err) return next(err)
+      //   if (
+      //     res.statusCode === 404 || (
+      //     res.headers['x-amz-meta-syncfilehash'] &&
+      //     res.headers['x-amz-meta-syncfilehash'] !== details.md5
+      //   )) return uploadFile(details, next)
+      //   if (res.statusCode >= 300) return next(new Error('Bad status code: ' + res.statusCode))
+      //   return next(null, details)
+      // })
     }
   }
 
@@ -102,7 +111,7 @@ function s3syncer(db, options) {
       , relative = prefix + details.path
       , lasterr
       , off = backoff.fibonacci({
-        initialDelay: 1000
+        initialDelay: 500
       })
 
     details.fresh = true
@@ -111,22 +120,33 @@ function s3syncer(db, options) {
     off.on('fail', function() {
       next(lasterr || new Error('unknown error'))
     }).on('ready', function() {
-      var headers = xtend({
-          'x-amz-acl': 'public-read'
-        , 'x-amz-meta-syncfilehash': details.md5
-        , 'Content-Type': mime.lookup(absolute)
-      }, options.headers)
+      // var headers = xtend({
+      //     'x-amz-acl': 'public-read'
+      //   , 'x-amz-meta-syncfilehash': details.md5
+      //   , 'Content-Type': mime.lookup(absolute)
+      // }, options.headers)
 
-      client.putFile(absolute, relative, headers, function(err, res) {
+      // client.putFile(absolute, relative, headers, function(err, res) {
+      //   if (!err) {
+      //     if (res.statusCode < 300) return next(null, details)
+      //     err = new Error('Bad status code: ' + res.statusCode)
+      //   }
+
+      //   lasterr = err
+      //   stream.emit('fail', err)
+      //   off.backoff()
+      // })
+
+      client.upload(absolute, relative, function(err, res) {
         if (!err) {
           if (res.statusCode < 300) return next(null, details)
           err = new Error('Bad status code: ' + res.statusCode)
         }
-
         lasterr = err
         stream.emit('fail', err)
         off.backoff()
       })
+
     }).backoff()
   }
 
